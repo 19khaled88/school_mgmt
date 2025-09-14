3f744b236a486868c446facbc0d2c2798627d844
19khaled88 <kkhaled88hasan@gmail.com>
2025-09-15 00:41:55 +0600

    modified eventCalendar, added announcement component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -27,7 +27,7 @@ export default function DashboardLayout({
         <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] b p-4">
           <Link href="/" className="flex items-center justify-center lg:justify-start gap-1">
             <Image src="/school-management-logo.png" alt="logo" width={80} height={80}/>
-            <span className="hidden lg:block"> School-Mgmt</span>
+            <span className="hidden lg:block font-bold"> School-Mgmt</span>
           </Link>
           <Menu />
         </div>

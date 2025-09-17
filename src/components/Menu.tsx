0c26dae2ca49e2669a13914234781bb9f4a82bf0
19khaled88 <kkhaled88hasan@gmail.com>
2025-09-17 23:13:00 +0600

    added bigcalendar, eventcalendar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from '@/lib/data'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -10,61 +11,75 @@ const menuItems = [
                 icon:'/home.png',
                 label:'Home',
                 href:'/',
+                visible:['admin','teacher','student','parent']
             },
             {
                 icon:'/teacher.png',
                 label:'Teachers',
-                href:'/teachers',  
+                href:'/list/teachers',  
+                visible:['admin','teacher']
             },
             {
                 icon:'/student.png',
                 label:'Students',
-                href:'/students',  
+                href:'/list/students',  
+                visible:['admin','teacher']
             },
             {
                 icon:'/parent.png',
                 label:'Parents',
-                href:'/parents',  
+                href:'/list/parents',  
             },
             {
                 icon:'/class.png',
                 label:'Classes',
-                href:'/classes',  
+                href:'/list/classes',  
             },
             {
                 icon:'/lesson.png',
                 label:'Lessons',
-                href:'/lessons',  
+                href:'/list/lessons',  
             },
             {
                 icon:'/exam.png',
                 label:'Exams',
-                href:'/examps',  
+                href:'/list/examps',  
             },
             {
                 icon:'/assignment.png',
                 label:'Assignments',
-                href:'/assignments',  
+                href:'/list/assignments',  
+                visible:['admin','teacher','student','parent']
+            },
+            {
+                icon:'/result.png',
+                label:'Results',
+                href:'/list/results',  
+                visible:['admin','teacher','student','parent']
             },
             {
                 icon:'/attendance.png',
                 label:'Attendance',
-                href:'/attendance',  
+                href:'/list/attendance',  
+                visible:['admin','teacher','student','parent']
             },
             {
                 icon:'/calendar.png',
                 label:'Events',
-                href:'/events',  
+                href:'/list/events',  
+                visible:['admin','teacher','student','parent']
             },
             {
                 icon:'/message.png',
                 label:'Messages',
-                href:'/messages',  
+                href:'/list/messages',  
+                visible:['admin','teacher','student','parent']
             },
             {
                 icon:'/announcement.png',
                 label:'Announcements',
-                href:'/announcements',  
+                href:'/list/announcements',  
+                visible:['admin','teacher','student','parent']
             }
         ]
     },
@@ -74,17 +89,20 @@ const menuItems = [
             {
                 icon:"/profile.png",
                 label:'Profile',
-                href:'/profile'
+                href:'/profile',
+                visible:['admin','teacher','student','parent']
             },
             {
                 icon:'/setting.png',
                 label:'Settings',
                 href:'/settings',  
+                visible:['admin','teacher','student','parent']
             },
             {
                 icon:'/logout.png',
                 label:'Logout',
                 href:'/logout',  
+                visible:['admin','teacher','student','parent']
             }
         ]
     }
@@ -95,14 +113,20 @@ const Menu = () => {
         {
             menuItems.map(i=>(
                 <div className='flex flex-col gap-2' key={i.title}>
-                    <span className='hidden lg:block text-gray-400 font-light my-4'>{i.title}</span>
+                    <span className='hidden lg:block text-gray-400 font-light my-4'>
+                        {i.title}
+                    </span>
                     {
-                        i.items.map(item=>(
-                            <Link href={item.href} key={item.label} className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2'>
-                                <Image src={item.icon} alt='' width={20} height={20}/>
-                                <span className='hidden lg:block'>{item.label}</span>
-                            </Link>
-                        ))
+                        i.items.map((item)=>{
+                            if(item.visible?.includes(role)){
+                                return(
+                                    <Link href={item.href} key={item.label} className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-blue-200'>
+                                        <Image src={item.icon} alt='' width={20} height={20}/>
+                                        <span className='hidden lg:block'>{item.label}</span>
+                                    </Link>
+                                    )
+                            }
+                        })
                     }
                 </div>
             ))
---
+++ src/lib/data.ts
@@ -0,0 +1,64 @@
+export let role = 'admin';
+
+export const teachersData =[
+
+];
+
+export const studentsData =[
+    
+];
+
+export const parentsData =[
+    
+];
+
+export const subjectsData =[
+    
+];
+
+export const classesData =[
+    
+];
+
+export const lessonsData =[
+    
+];
+
+export const examsData =[
+    
+];
+
+export const assignmentsData =[
+    
+];
+
+export const annoucementsData = [
+
+];
+
+export const calendarEvents = [
+    {
+        title:'Math',
+        allDay:false,
+        start:new Date(2024, 7, 12, 8, 0),
+        end:new Date(2024, 7, 12, 8, 45)
+    },
+    {
+        title:'English',
+        allDay:false,
+        start:new Date(2024, 7, 12, 9, 0),
+        end:new Date(2024, 7, 12, 9, 45)
+    },
+    {
+        title:'Biology',
+        allDay:false,
+        start:new Date(2024, 7, 12, 10, 0),
+        end:new Date(2024, 7, 12, 10, 45)
+    },
+    {
+        title:'Physics',
+        allDay:false,
+        start:new Date(2024, 7, 12, 11, 0),
+        end:new Date(2024, 7, 12, 11, 45)
+    },
+]

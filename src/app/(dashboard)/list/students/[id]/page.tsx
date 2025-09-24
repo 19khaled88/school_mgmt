8608eb224ffd601ba9f850820bd92737a59b5524
19khaled88 <kkhaled88hasan@gmail.com>
2025-09-24 07:54:36 +0600

    added form_modal

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -18,7 +18,7 @@ const SingleTeacherPage = () => {
                             <Image src="https://images.openai.com/thumbnails/url/2jOY1nicu1mUUVJSUGylr5-al1xUWVCSmqJbkpRnoJdeXJJYkpmsl5yfq5-Zm5ieWmxfaAuUsXL0S7F0Tw5JDTMoyDbxNUupCEgKyC1zzikMrvIodIrKd9YtL3FOzo1PywizqNINK8oKSzMvLcoJ8XJ19C8q9PBUKwYA4NAqAQ" alt='' width={144} height={144} className='w-36 h-36 rounded-full object-cover' />
                         </div>
                         <div className='w-2/3 flex flex-col justify-between gap-4'>
-                            <h1>Leonard Snyder</h1>
+                            <h1>Orobindo disylba</h1>
                             <p className='text-sm text-gray-500'>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                             </p>
@@ -56,15 +56,15 @@ const SingleTeacherPage = () => {
                         <div className='bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]'>
                             <Image src="/singleBranch.png" alt='' width={24} height={24} className='w-6 h-6' />
                             <div className=''>
-                                <h1 className='text-xl font-semibold'>2</h1>
-                                <span className='text-sm text-gray-400'>Branches</span>
+                                <h1 className='text-xl font-semibold'>6th</h1>
+                                <span className='text-sm text-gray-400'>Grade</span>
                             </div>
                         </div>
                         {/*CARD*/}
                         <div className='bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]'>
                             <Image src="/singleLesson.png" alt='' width={24} height={24} className='w-6 h-6' />
                             <div className=''>
-                                <h1 className='text-xl font-semibold'>6</h1>
+                                <h1 className='text-xl font-semibold'>18</h1>
                                 <span className='text-sm text-gray-400'>Lessons</span>
                             </div>
                         </div>
@@ -72,8 +72,8 @@ const SingleTeacherPage = () => {
                         <div className='bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]'>
                             <Image src="/singleClass.png" alt='' width={24} height={24} className='w-6 h-6' />
                             <div className=''>
-                                <h1 className='text-xl font-semibold'>6</h1>
-                                <span className='text-sm text-gray-400'>Classes</span>
+                                <h1 className='text-xl font-semibold'>6A</h1>
+                                <span className='text-sm text-gray-400'>Class</span>
                             </div>
                         </div>
 
@@ -81,7 +81,7 @@ const SingleTeacherPage = () => {
                 </div>
                 {/*BOTTOM*/}
                 <div className='mt-4 bg-white rounded-md p-4 h-[800px]'>
-                    <h1>Teacher&apos;s Schedule</h1>
+                    <h1>Student&apos;s Schedule</h1>
                     <BigCalendar />
                 </div>
             </div>
@@ -90,11 +90,11 @@ const SingleTeacherPage = () => {
                 <div className='bg-white p-4 rounded-md'>
                     <h1 className='text-xl font-semibold'>Shortcuts</h1>
                     <div className='mt-4 flex gap-4 flex-wrap text-xs text-gray-500'>
-                        <Link className='p-3 rounded-md bg-blue-200' href="/">Teacher&apos;s Classes</Link>
-                        <Link className='p-3 rounded-md bg-purple-200' href="/">Teacher&apos;s Students</Link>
-                        <Link className='p-3 rounded-md bg-yellow-200' href="/">Teacher&apos;s Lessons</Link>
-                        <Link className='p-3 rounded-md bg-pink-200' href="/">Teacher&apos;s Exams</Link>
-                        <Link className='p-3 rounded-md bg-blue-200' href="/">Teacher&apos;s Assignments</Link>
+                        <Link className='p-3 rounded-md bg-blue-200' href="/">Student&apos;s Lessons</Link>
+                        <Link className='p-3 rounded-md bg-purple-200' href="/">Student&apos;s Teachers</Link>
+                        <Link className='p-3 rounded-md bg-yellow-200' href="/">Student&apos;s Results</Link>
+                        <Link className='p-3 rounded-md bg-pink-200' href="/">Student&apos;s Exams</Link>
+                        <Link className='p-3 rounded-md bg-blue-200' href="/">Student&apos;s Assignments</Link>
                     </div>
                 </div>
                 <Performance />
